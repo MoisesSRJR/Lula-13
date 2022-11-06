5db2d7565a0f9a2043bb7e69331fde83d8540001
Wellinton Reis <wellinton.reis@uds.com.br>
2022-11-06 11:47:32 -0300

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lulapresidente.app',
+  appName: 'lula-presidente',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
